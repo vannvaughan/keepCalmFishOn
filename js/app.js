890f2baf935b5885d7d3baefccbc2fa5f23a6777
vannvaughan <vann.vaughan@yahoo.com>
2017-06-13 09:37:19 -0500

    created new nev bar and content

--- js/app.js
+++ js/app.js
@@ -11,20 +11,20 @@ $urlRouterProvider.otherwise('/')
 $stateProvider
     .state('about',{
         url: '/',
-        templateUrl: '/views/about.html',
+        templateUrl: 'views/about.html',
         controller: 'mainCtrl',
     })
     .state('howToHelp', {
         url: '/howToHelp',
-        templateUrl: '/views/howToHelp.html'
+        templateUrl: 'views/howToHelp.html'
     })
     .state('shop', {
         url: '/shop',
-        templateUrl: '/views/shop.html',
+        templateUrl: 'views/shop.html',
     })
     .state('contact', {
         url: '/contact',
-        templateUrl: '/views/contact.html'
+        templateUrl: 'views/contact.html'
     })
 
 })
--- js/jquery/animations.js
+++ js/jquery/animations.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-  
-    
+
+$(".container").click(function() {
+    $(".stick").toggleClass(function () {
+        return $(this).is('.open, .close') ? 'open close' : 'open';
+    });
+});
+
+$(".nav-bar").hide();
+
+$(".container").click(function(){
+    $(".nav-bar").slideToggle(500, function(){
+        
+    });
+});
+
+
+
+
 
 });
+
